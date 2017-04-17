bcd50a76f0ce3fb725417d03f5938fe195d5d026
Petar Zaninovich <pzaninov@ucsc.edu>
2017-04-17 15:10:13 -0700

    updates
    remove and add User Cal made

--- js/db.js
+++ js/db.js
@@ -55,32 +55,73 @@ function deleteUser(userID){
         showUsers();
 
 }
+// the || is used as a concatenation operator in sqlite
 function addUserCal(userID, bTimeStart, bTimeEnd, bDayStart, bDayEnd){
+	bTimeStart = "" + "," + bTimeStart;
+	bTimeEnd = "" + ","   + bTimeEnd;
+	bDayStart = "" + ","  + bDayStart;
+	bDayEnd = "" + ","    + bDayEnd;
     db.transaction(function(tx){
         tx.executeSql("UPDATE USERTABLE SET bTimeStart   = bTimeStart || '"+bTimeStart+"', \
                                             bTimeEnd     = bTimeEnd   || '"+bTimeEnd  +"', \
                                             bDayStart    = bDayStart  || '"+bDayStart +"', \
-                                            bDayEnd      = bTimeStart || '"+bDayEnd   +"'  \
+                                            bDayEnd      = bDayEnd || '"+bDayEnd   +"'  \
                                             WHERE userID = "+ userID);
         });
         console.log("We are in addUsersCal");
         showUsers();
 
 }
+
+
 function addUserGroups(userID,groupID){
     db.transaction(function(tx){
-        tx.executeSql("UPDATE USERTABLE SET groupID = '"+groupID +"' WHERE userID = "+ userID);
+        tx.executeSql("UPDATE USERTABLE SET groupID = groupID ||'"+groupID +"' WHERE userID = "+ userID);
     });
     console.log("We are in addUsersGroup");
     showUsers();
 }
+//this does not remove the commas that seperates the data
+function removeUserCal(userID, bTimeStart, bTimeEnd, bDayStart, bDayEnd){
+	var upbts = "";
+	var upbte = "";
+	var upbds = "";
+	var upbde = "";
+	    db.transaction(function(tx){
+        console.log(tx);
+        tx.executeSql("SELECT userID, userName, bTimeStart, \
+                       bTimeEnd, bDayStart, bDayEnd, groupID FROM USERTABLE", [], function(tx,result){
+            
+            for(var i = 0; i< result.rows.length;i++){
+                var row = result.rows.item(i);
+				upbts = row['bTimeStart'].replace(bTimeStart,"");
+				upbte = row['bTimeEnd'].replace(bTimeEnd,"");
+				upbds = row['bDayStart'].replace(bDayStart,"");
+				upbde = row['bDayEnd'].replace(bDayEnd,"");
+				
 
+            }
+        });
+        
+    });
+	console.log(upbts,upbte,upbds,upbde);
+	db.transaction(function(tx){
+        tx.executeSql("UPDATE USERTABLE SET bTimeStart   = '"+upbts  +"', \
+                                            bTimeEnd     = '"+upbte  +"', \
+                                            bDayStart    = '"+upbds  +"', \
+                                            bDayEnd      = '"+upbde  +"'  \
+                                            WHERE userID = "+ userID);
+        });
+}
 function doAll(){
     openUserDatabase();
     createUserTable();
-    createUser(1234,"afhenry","ap","b","c","d","e");
+    createUser(1234,"afhenry","a","b","c","d","e");
     
-    addUserCal(1234,"lol",2,2,2);
+    addUserCal(1234,"bts","bte","bds","bde");
+	
+    removeUserCal(1234,"a","b","c","d");
+	showUsers();
     addUserGroups(1234,"poobar");
     deleteUser(1234);
 }
