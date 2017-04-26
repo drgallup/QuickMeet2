9b754b0912e1aad9999b644c4a6f64f3fe19c7d1
Kyle Remmert <kremmert@ucsc.edu>
2017-04-26 11:53:07 -0700

    Added menu
    Added a menu to the page, with broken links to home and about pages.

--- public/js/angularController.js
+++ public/js/angularController.js
@@ -1,7 +1,7 @@
 // controller.js
 
-angular.module('MyApp', ['ngMaterial'])
-.controller('appCtrl', function($scope) {
+var app = angular.module('MyApp', ['ngMaterial']);
+app.controller('appCtrl', function($scope) {
   $scope.data = {
     cb1: true,
     cb4: true,
@@ -9,11 +9,34 @@ angular.module('MyApp', ['ngMaterial'])
   };
 
   $scope.message = 'false';
-  
-  //$scope.greeting = { text: 'Hello' };
-  //$scope.date = Date('2017', '04', '20');
 
   $scope.onChange = function(cbState) {
   	$scope.message = cbState;
   };
+ 
+});
+
+app.controller('SidebarController', function($scope) 
+{
+    $scope.state = false;
+    $scope.toggleState = function() 
+    {
+        $scope.state = !$scope.state;
+    };
+    
+});
+
+app.directive('sidebarDirective', function() {
+    return {
+        link : function(scope, element, attr) {
+            scope.$watch(attr.sidebarDirective, function(newVal) {
+                  if(newVal)
+                  {
+                    element.addClass('show'); 
+                    return;
+                  }
+                  element.removeClass('show');
+            });
+        }
+    };
 });
