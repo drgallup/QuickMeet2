c30fab5a8c2647184150b16b696c9fffc5079959
drgmonkey <dylangallup@gmail.com>
2017-05-29 13:25:52 -0700

    edit controller for group page

--- public/js/angularController.js
+++ public/js/angularController.js
@@ -21,6 +21,22 @@ app.controller('PopCtrl', function($scope, $mdDialog) {
     });
   };	
   
+  $scope.showGroup = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'dialog1.group.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };	
+  
   function DialogController($scope, $mdDialog) {
     $scope.hide = function() {
       $mdDialog.hide();
